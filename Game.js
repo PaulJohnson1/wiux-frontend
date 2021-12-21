3583020f2117f42b564e38cb982ab4d2b70ab4b9
Paul Johnson <93623067+PaulJohnson1@users.noreply.github.com>
2021-12-21 21:45:25 +0000

    www

--- Game.js
+++ Game.js
@@ -35,8 +35,9 @@ export default class Game {
 
     this.ctx = canvas.getContext("2d");
 
-    if (location.href.includes("wiux.io")) this.socket = new WebSocket("wss://wiux.io/socket/"); // production server
+    if (location.href.includes("wiux.io")) this.socket = new WebSocket("wss://www.wiux.io/socket/"); // production server
     else this.socket = new WebSocket("wss://wiux-backend.pauljohnson11.repl.co"); // development server
+
     this.socket.binaryType = "arraybuffer";
     this.socket.bitsRecieved = 0;
     this.socket.packetsRecieved = 0;
